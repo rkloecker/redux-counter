372db3c0b26dcda8c3ccea589c23c46b1f7fbe04
R K <blah@eample.com>
2019-04-11 13:59:55 +0200

    Updates

---
+++ precache-manifest.429efb1ca411e5f73fc16974fd6a3cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0078c8eb852a8f895f9",
+    "url": "/redux-counter/static/js/runtime~main.125d2423.js"
+  },
+  {
+    "revision": "e2561fc2d8d8cd379dfd",
+    "url": "/redux-counter/static/js/main.f113f4d4.chunk.js"
+  },
+  {
+    "revision": "7492826f7e6dc31f77a2",
+    "url": "/redux-counter/static/js/2.73614738.chunk.js"
+  },
+  {
+    "revision": "e2561fc2d8d8cd379dfd",
+    "url": "/redux-counter/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "7492826f7e6dc31f77a2",
+    "url": "/redux-counter/static/css/2.0e3883e2.chunk.css"
+  },
+  {
+    "revision": "15c79a03d70b0c8fbec4bffbba825680",
+    "url": "/redux-counter/index.html"
+  }
+];
